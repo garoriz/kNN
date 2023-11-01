d4e6dcf1a65e09143891d6d052bcf158620a4e53
rizvan <rizvangaripov@gmail.com>
2023-11-01 12:54:16 +0300

    added algorithm

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ class Iris:
         self.petal_width = petal_width
 
 
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+
+def k_nearest_neighbors(X_train, y_train, x_test, k):
+    distances = []
+    for i in range(len(X_train)):
+        distance = euclidean_distance(x_test, X_train[i])
+        distances.append((distance, y_train[i]))
+    distances.sort(key=lambda x: x[0])
+    neighbors = [distance[1] for distance in distances[:k]]
+    prediction = max(set(neighbors), key=neighbors.count)
+    return prediction
+
+
 if __name__ == '__main__':
     iris = load_iris()
     X = iris.data
@@ -43,3 +58,5 @@ if __name__ == '__main__':
 
     X_test = [X[i] for i in indices[num_train_samples:]]
     y_test = [y[i] for i in indices[num_train_samples:]]
+
+    optimal_k = np.sqrt(len(X))
